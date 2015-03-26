6d5ada4b8f413e644f1efacf66a1d98324b3cea8
Zhang Zhuo Bin <zhuobin.zhang@schoolface.cn>
2015-03-26 17:56:41 +0800

    Build

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp')
+var jade = require('gulp-jade')
+var browserSync = require('browser-sync')
+
+
+var reload=browserSync.reload
+var paths={
+    html:"build",
+    js:"build",
+
+    jade:"src/jade/*.jade",
+    script:["src/js/*.js","bower_components/jquery/dist/jquery.min.js"]
+}
+
+gulp.task("browser-sync",function(){
+    browserSync({
+        server:{
+            baseDir:"build"
+        }
+    })
+    gulp.watch(paths.jade,["html",reload])
+    gulp.watch(paths.script,["js",reload])
+})
+gulp.task("html",function(){
+    gulp.src(paths.jade)
+        .pipe(jade())
+        .pipe(gulp.dest(paths.html))
+})
+
+gulp.task("js",function(){
+    gulp.src(paths.script)
+        .pipe(gulp.dest(paths.js))
+})
+
+gulp.task("default",function(){
+    gulp.run("html","js","browser-sync")
+})
